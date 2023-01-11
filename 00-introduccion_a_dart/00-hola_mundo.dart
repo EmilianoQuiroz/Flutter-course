c5de7653ed34cdeaf1fc801a37d46941840aed35
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-11 01:19:48 -0300

    Hola mundo en Dart

---
+++ 00-introduccion_a_dart/00-hola_mundo.dart
@@ -0,0 +1,13 @@
+//***** Hola mundo en Dart ******//
+/*En Dart contamos con una funcion principal main*/
+main()
+{
+  print('Hola mundo');/*Imprimimos en pantalla*/
+
+  /*Comentarios*/
+  /*
+  Comentario
+  multilinea
+  */
+  //Comentario de una sola linea
+}
