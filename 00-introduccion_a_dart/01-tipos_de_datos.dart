58f8f93e77d8f8d5bb9d34a647bb8153ca9b3215
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-11 01:26:58 -0300

    Tipos de datos en Dart

---
+++ 00-introduccion_a_dart/01-tipos_de_datos.dart
@@ -0,0 +1,22 @@
+//***** Tipos de datos en Dart ******/
+main()
+{
+  //Strings
+  var nombre = 'Santiago';/*Cuando creamos nuestra variable con var esta detecta el tipo de dato que le introducimos, es recomendable no usarla*/
+  String apellidos = 'Quiroz';/*Podemos asignar el tipo de dato en este caso es una cadena de texto*/
+
+  print(nombre);
+  print(apellidos);
+
+  //Enteros
+  int numeroEntero = 34;
+
+  print(numeroEntero);
+
+  //Flotantes
+  double numeroFlotante = 3.5;
+
+  print(numeroFlotante);
+
+
+}
